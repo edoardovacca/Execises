d1e03d163a31935104abaf93c30fecdb1b793e61
edoardovacca <45948678+edoardovacca@users.noreply.github.com>
2022-12-05 11:57:31 +0200

    Delete 09_More_Array_Tasks directory

--- 09_More_Array_Tasks/09.arrays_more_tasks_1.js
+++
@@ -1,8 +0,0 @@
-var wordArray = ["Actions speak louder than words", "All for one and one for all.",
-        "Don't fire until you see the whites of their eyes.", "Give him an inch and he'll take a mile.", "He who hesitates is lost."];
-var randomNumber =  Math.floor(Math.random() * 4);  
-
-var outputText= document.getElementById("pOutput");
-outputText.innerHTML= wordArray[randomNumber];
-
-// End
