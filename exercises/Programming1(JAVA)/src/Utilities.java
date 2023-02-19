d524a205f5e11a6d8681ed4c34e35463030835be
edoardovacca <45948678+edoardovacca@users.noreply.github.com>
2023-02-19 12:22:03 +0200

    Delete exercises/Programming1(JAVA)/src directory

--- exercises/Programming1(JAVA)/src/Utilities.java
+++
@@ -1,9 +0,0 @@
-
-public class Utilities {
-	
-	//this is not a program but other java classes want to use the methods that are in this class
-	//so you need to change the original private static double from MethodExample.java class into a public statci void
-	public static double computeSum(double a, double b) {
-		return a + b;
-	}
-}
